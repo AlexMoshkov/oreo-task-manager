dfed9b8377c07d52a133b822716c27c41a0530eb
AlexMoshkov <9aq360zqmuf1@mail.ru>
2022-08-06 16:12:57 +0500

    update backend structure

--- app/server.py
+++ app/server.py
@@ -1,6 +1,7 @@
-from typing import Union
-
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+from app.dto.requests import CardIn, ColumnIn, CheckerInfoIn
 
 app = FastAPI()
 
@@ -8,3 +9,48 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/api/card/{id}")
+async def get_card(id: str) -> JSONResponse:
+    ...
+
+
+@app.post("/api/card")
+async def create_card(data: CardIn) -> JSONResponse:
+    ...
+
+
+@app.put("/api/card/{id}")
+async def update_card(id: str, data: CardIn) -> JSONResponse:
+    ...
+
+
+@app.delete("/api/card/{id}")
+async def delete_card(id: str) -> JSONResponse:
+    ...
+
+
+@app.get("/api/column/{id}")
+async def get_column(id: str) -> JSONResponse:
+    ...
+
+
+@app.post("/api/column")
+async def create_column(data: ColumnIn) -> JSONResponse:
+    ...
+
+
+@app.put("/api/column/{id}")
+async def update_column(id: str, data: ColumnIn) -> JSONResponse:
+    ...
+
+
+@app.delete("/api/column/{id}")
+async def delete_column(id: str) -> JSONResponse:
+    ...
+
+
+@app.post("/api/checker")
+async def get_checker_info(data: CheckerInfoIn):
+    ...
