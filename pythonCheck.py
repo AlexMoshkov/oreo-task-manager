ea782832c63c5b38cb819d10ac3f5adfb7c8e710
Ivan Savyak <alisiama@student.21-school.ru>
2022-08-06 13:09:37 +0300

    dsfd

--- pythonCheck.py
+++ pythonCheck.py
@@ -1,28 +1,36 @@
 from codecs import backslashreplace_errors
 import socket
 import re
+# import python3-nmap
 
+def check_internet():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex(('8.8.8.8', 53))
 
+def check_host(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(5)
+    result = sock.connect_ex((host ,int(port)))
+    if result == 0:
+#              backend.send("OK");
+        print(host + port +  ':OK')
+    else:
+#          backend.send("BAD");
+        print(host + port +':BAD')
 
 def check_ports_from_file(filename):
     with open(filename, 'r') as f:
             for line in f:
-                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                if line != "":
                     host, port = line.split(":")[0], line.split(":")[1]
-
-                    result = sock.connect_ex((host ,int(port)))
-                    if result == 0:
-#                    backend.send("OK");
-                        print(line + ':OK');
-                    else:
-    #                    backend.send("BAD");
-                        print(line + ':BAD');
+                    check_host(host, port);
 
 
 
 def main():
-    filename = 'hostlist.txt';
-    check_ports_from_file(filename);
+    filename = 'hostlist.txt'
+    check_internet()
+    check_ports_from_file(filename)
 
 if __name__ == '__main__':
     main()
