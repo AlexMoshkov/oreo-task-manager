8cba1625bd4b4d4c561bfa7267e809cfdc1a5fa2
Ivan Savyak <alisiama@student.21-school.ru>
2022-08-07 06:46:06 +0300

    cyber

--- pythonCheck.py
+++ pythonCheck.py
@@ -36,10 +36,13 @@ def check_host(host, port):
 
 def fetch_hosts():
     # DEBUGURL = DEBUGURL + "hostlist"
-    hosts = requests.get(DEBUGURL + "hostlist/")
-    print(hosts)
-    hosts = hosts.json()
-    return hosts
+    try:
+        hosts = requests.get(DEBUGURL + "hostlist")
+        print(hosts)
+        hosts = hosts.json()
+        return hosts
+    except:
+        return "requestExseptions"
 
 
 
@@ -48,47 +51,53 @@ def send_to_backend(hash, result): #result bool True/False
     print(result)
     data = {"key": hash, "status": result}
     data_json = json.dumps(data)
+    print(data_json)
     # DEBUGURL = DEBUGURL + "checker/"
-    r = requests.post(DEBUGURL + "checker/", data = data_json)
-    print(r)
-    print(r.json())
-    if r.status_code == 200: print("Success send hosts!")
+    try:
+        r = requests.post(DEBUGURL + "checker", data = data_json)
+        print(r)
+        print(r.json())
+        if r.status_code == 200: print("Success send hosts!")
+    except:
+        pass
 
-def check_ports_from_file(filename):
-    for line in fetch_hosts():
-        result = -1
-        hash = line["id"]
-        # print("hash:  " + str(hash))
-        line = (line["host"])
-        line = line.replace('http://' ,'')
-        line = line.replace('https://' ,'')
-        line = line.replace('\n', '')
-        if ":" in line:
-            host, port = line.split(":")[0], line.split(":")[1]
-        else:
-            host = line.replace('\n', '')
-            port = 80
-        result = check_host(host, port) #new tread and go next without delay
-        send_to_backend(hash, result) # delete host
-        if result == 1:
-#           backend.send("OK");
-            print(line +  '   :OK')
-            pass
-        else:
-#          backend.send("BAD");
-            print(line +'   :BAD')
+def check_ports():
+    text = fetch_hosts()
+    if text != "requestExseptions":
+        for line in text:
+            result = -1
+            hash = line["id"]
+            # print("hash:  " + str(hash))
+            line = (line["host"])
+            line = line.replace('http://' ,'')
+            line = line.replace('https://' ,'')
+            line = line.replace('\n', '')
+            if ":" in line:
+                host, port = line.split(":")[0], line.split(":")[1]
+            else:
+                host = line.replace('\n', '')
+                port = 80
+            result = check_host(host, port) #new tread and go next without delay
+            send_to_backend(hash, result) # delete host
+            if result == 1:
+    #           backend.send("OK");
+                print(line +  '   :OK')
+                pass
+            else:
+    #          backend.send("BAD");
+                print(line +'   :BAD')
 
 def main():
-    filename = 'hostlist.txt'
-    fetch_hosts()
-    send_to_backend("1234", True)
-    check_internet()
+    check_ports()
+    # filename = 'hostlist.txt'
+    # fetch_hosts()
+    # send_to_backend("1234", True)
+    # check_internet()
 
-    if check_host("www.yandex.ru", 80): print('ok')
-    if check_host('yandex.ru', 80): print('ok')
-
-    if check_host("letoctf.org", 80): print('ok')
-    check_ports_from_file(filename)
+    # if check_host("www.yandex.ru", 80): print('ok')
+    # if check_host('yandex.ru', 80): print('ok')
+    # if check_host("letoctf.org", 80): print('ok')
+    # check_ports(filename)
 
 if __name__ == '__main__':
     main()
